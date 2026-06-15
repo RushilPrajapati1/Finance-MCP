282b4601968c9a83c56dfa574bc11892c0a648a4
RushilPrajapati1 <rushilvprajapati@gmail.com>
2026-06-14 21:56:35 -0400

    startup app

--- app/main.py
+++ app/main.py
@@ -87,3 +87,9 @@ def create_app() -> FastAPI:
 
 
 app = create_app()
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
